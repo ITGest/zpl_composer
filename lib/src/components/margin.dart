67d7543250b3884252dcc7b12f0ab0b6971f7253
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-06 13:36:42 +0100

    Added margin compoenent

---
+++ lib/src/components/margin.dart
@@ -0,0 +1,21 @@
+import 'package:zpl_composer/src/composer.dart';
+
+class Margin implements ZplComposer {
+  @override
+  ZplComposer build([ZplComposer parent]) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  @override
+  ZplComposer fromString(String zplString) {
+    // TODO: implement fromString
+    throw UnimplementedError();
+  }
+
+  @override
+  String getString() {
+    // TODO: implement getString
+    throw UnimplementedError();
+  }
+}
