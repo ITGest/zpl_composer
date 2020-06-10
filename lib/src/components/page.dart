51efe561e739b7222c2f4cfe03f906cd987be06f
Eleandro Duzentos <eleandro@inbox.ru>
2020-06-10 10:16:28 +0100

    Added with property

--- lib/src/components/page.dart
+++ lib/src/components/page.dart
@@ -1,19 +1,28 @@
+import 'package:zpl_composer/src/components/font.dart';
 import 'package:zpl_composer/src/composer.dart';
 
 class Page implements ZplComposer {
-  String _zplString = '';
-
+  final int width;
   final ZplComposer child;
 
+  String _zplString = '';
+
   Page({
+    this.width,
     this.child,
   });
 
   @override
   ZplComposer build([ZplComposer parent]) {
-    final _childZplString = child.getString();
+    String _content = '';
 
-    _zplString = '^XA$_childZplString^XZ';
+    if (!(child is Font)) {
+      _content = Font().getString();
+    }
+
+    _content += child.getString();
+
+    _zplString = '^XA$_content^XZ';
 
     return this;
   }
